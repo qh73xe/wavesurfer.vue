2f79f66b33069d61e6db0186d538a5dde45fabc2
takuya.asai <takuya.asai@miidas.jp>
2021-12-27 14:39:00 +0900

    デモページの復旧

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import AppBar from "./AppBar.js";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    app_name: "wavesurfer.vue",
+    github: "https://github.com/qh73xe/wavesurfer.vue",
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: { app_bar: AppBar },
+});
